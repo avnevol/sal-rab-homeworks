13f11ecff60be533abb4023b4c8d935d67006be1
avnevol <avnevolin@gmail.com>
2021-11-05 18:38:42 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,16 +1,26 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message;
+    message = productName;
+    message = message + ` `;
+    message = message + `за`;
+    message = message + ` `;
+    message = message + productPrice;
+    message = message + ` `;
+    message = message + `теперь в корзине!`;
+    console.log(message);
+    
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
+    // создайте переменную message +
+    // присвойте переменной message значение переменной productName +
+    // добавьте в конец переменной message пробел +
+    // добавьте в конец переменной message текст 'за' +
+    // добавьте в конец переменной message пробел +
+    // добавьте в конец переменной message значение переменной productPrice +
+    // добавьте в конец переменной message пробел +
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
 
@@ -23,6 +33,9 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue;
+    newValue = oldValue;
+    newValue = newValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,7 +49,11 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
-
+    let newSum;
+    newSum = oldSum + difference;
+    let newSumText;
+    newSumText = newSum + ' ₽'; 
+    newSumText = `${newSum} ₽`;
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
