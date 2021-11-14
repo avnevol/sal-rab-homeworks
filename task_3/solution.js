9999f5d7064f3283ca35488a7b209d1f2c8060c8
avnevol <avnevolin@gmail.com>
2021-11-14 21:13:17 +0300

    task 4 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -20,7 +20,7 @@ function renderCartItem(item) {
     itemCountText += ` × `;
     itemCountText += product.price;
     itemCountText += ` ₽ = `;
-    let sum = product.count*product.price;
+    let sum = product.count * product.price;
     itemCountText += sum;
     itemCountText += ` ₽`;
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
--- task_4/solution.js
+++ task_4/solution.js
@@ -1,6 +1,9 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
+    let data = JSON.parse(json);
+    let products = data.products;
+    return products;
     // Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
@@ -8,6 +11,17 @@ function parseProducts(json) {
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
 }
+    function renderProductsCards(json){
+    clearProducts();
+    let products;
+    products = parseProducts(json);
+    length = products.length;
+    for (let i = 0; i < length; i += 1) {
+        // место для пункта 7
+        addProduct(products[i]); 
+    }
+     // дальнейшие действия требуется выполнять тут
+ }
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
